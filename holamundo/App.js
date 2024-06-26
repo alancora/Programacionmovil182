8f1e4f8f96681745b4bbda1b39fd170d23dc138c
alancora <158220402+alancora@users.noreply.github.com>
2024-06-26 15:01:20 -0600

    sectionlist practica11

--- holamundo/App.js
+++ holamundo/App.js
@@ -1,100 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, Dimensions } from 'react-native';
-import React, { useState, useRef } from 'react';
-
+import { StyleSheet, Text, View, SectionList } from 'react-native';
+import React from 'react';
 
 export default function App() {
-  const[text,setText]=useState('valor Default')
-  const[submit,setSubmit]=useState('')
-
-
   return (
     <View style={styles.container}>
-
-      <ScrollView style={styles.ScrollView}>
-
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-      <Text>Componente TextInput: {submit}</Text>
-
-
-      <TextInput style={styles.input} placeholder='Escribe texto....' onChangeText={(t)=>setText(t)} defaultValue={text} />
-
-      <Button title = 'Presioname...' onPress={()=>{ setSubmit(text);  alert('Texto Enviado')}}/>
-
-      </ScrollView>
-    
+      <SectionList
+        sections={[
+          {
+            title: 'Grupo A',
+            data: [
+              { key: '1', name: 'Alan' },
+              { key: '2', name: 'Cristian' },
+              { key: '3', name: 'Aldair' },
+              { key: '4', name: 'Josue' },
+            ],
+          },
+          {
+            title: 'Grupo B',
+            data: [
+              { key: '5', name: 'Luis' },
+              { key: '6', name: 'Caro' },
+              { key: '7', name: 'Monse' },
+              { key: '8', name: 'Majo' },
+            ],
+          },
+          {
+            title: 'Grupo C',
+            data: [
+              { key: '9', name: 'Valeria' },
+              { key: '10', name: 'Andres' },
+              { key: '11', name: 'Geraldine' },
+              { key: '12', name: 'Marcos' },
+            ],
+          },
+        ]}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+        renderSectionHeader={({ section }) => <Text style={styles.section}>{section.title}</Text>}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -103,23 +46,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection:'column',
+    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    paddingTop: 40,
   },
-  input:{
-    /*backgroundColor:'#dfd7fd',*/
-    width:'80%',
-    height: 40,
+  item: {
+    padding: 10,
+    fontSize: 24,
+    height: 50,
+    borderColor: 'blue',
     borderBottomWidth: 1,
-    borderBottomColor:'red',
-
   },
-
-  ScrollView:{
-    width:Dimensions.get('window').width
-
-  }
-
+  section: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    backgroundColor: 'red',
+    paddingTop: 4,
+    paddingBottom: 4,
+  },
 });
